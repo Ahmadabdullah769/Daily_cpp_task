d9c36119917d72bc2696bbcc4965d6dbaccf3248
Ahmadabdullah769 <Ahmadabdullahx2000@gmail.com>
2025-08-19 20:57:11 +0500

    endl Manipulator example

---
+++ Tuesday-19-8/Manipulator.cpp
@@ -0,0 +1,8 @@
+//endl manipulator
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"My"<<endl<<"name"<<endl<< "is"<<endl<< "Abdullah"<<endl<< "and"<<endl<< "we"<<endl<<"are"<<endl<<"studying"<<endl<<"computer"<<endl<<"science"<<endl;
+	return 0;
+}

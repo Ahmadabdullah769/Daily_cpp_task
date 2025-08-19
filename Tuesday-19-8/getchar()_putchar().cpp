a087077a1e914102ad7159ad0a43e056df0c1cc6
Ahmadabdullah769 <Ahmadabdullahx2000@gmail.com>
2025-08-19 20:39:23 +0500

    getchar() and putchar() example

---
+++ Tuesday-19-8/getchar()_putchar().cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	char ch;
+	cout<<"Enter character for gender\n";
+	ch=getchar();
+	cout<<"You have entered: ";
+	putchar(ch);
+	return 0;
+}
